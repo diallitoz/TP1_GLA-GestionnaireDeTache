8d8b96390cf8a8d5f77bee4c31d00a1157bba625
DIALLO Azise Oumar <diallitoz@gmail.com>
2017-08-21 03:15:54 +0700

    Add files via upload

--- TaskManager.java
+++ TaskManager.java
@@ -28,6 +28,7 @@ public class TaskManager {
 		// TODO Auto-generated method stub
 		String cmd = "o";
 		String choix = "0";
+		// int Id = 0;
 
 		Menu.displayWelcomeMsg();
 
@@ -35,75 +36,24 @@ public class TaskManager {
 			// affichage du menu principal
 			Menu.displayPrincipalMenu();
 			choix = keyb.nextLine();
-			String itemMenu = "0";
 			if (choix.equals("1")) {
 
-				// affichage du Menu pour la gestion des membres de l'equipe
-				
+				// Gestion des membres de l'equipe
+
 				mu.gererMembre(mList);
-				
 
 			} else if (choix.equals("2")) {
-				// affichage du Menu pour la gestion des taches
+
+				// Gestion des taches
 
 				tu.gererTache(tList, mList, aList);
-				
-				
+
 			} else if (choix.equals("3"))
 
 			{
-				// affichage du Menu pour la recherche des taches
-				boolean cmd3 = true;
-				do {
+				// Recherche des taches
 
-					Menu.displayTypeSearchTaskMenu();
-
-					itemMenu = keyb.nextLine();
-					if (itemMenu.equals("1")) {
-
-						System.out.println("taches assignees a un membre");
-
-					} else if (itemMenu.equals("2")) {
-
-						boolean cmd4 = true;
-						do {
-
-							Menu.displayTaskStatusMenu();
-							String itemMenu1 = keyb.nextLine();
-							if (itemMenu1.equals("1")) {
-								System.out.println("taches nouvelles");
-							} else if (itemMenu1.equals("2")) {
-								System.out.println("taches en progres");
-							} else if (itemMenu1.equals("3")) {
-								System.out.println("taches terminees");
-							} else if (itemMenu1.equals("4")) {
-								cmd4 = false;
-							} else if (itemMenu1.equals("5")) {
-								cmd4 = false;
-								cmd3 = false;
-								// Menu.displayPrincipalMenu();
-							}
-
-							else if (itemMenu1.equals("6")) {
-								quitterProgramme();
-							}
-
-							else if (!itemMenu1.equals("1") && !itemMenu1.equals("2") && !itemMenu1.equals("3")
-									&& !itemMenu1.equals("4") && !itemMenu1.equals("5") && !itemMenu1.equals("6")) {
-								choixIncorrect();
-							}
-
-						} while (cmd4);
-
-					} else if (itemMenu.equals("3")) {
-						cmd3 = false;
-					} else if (itemMenu.equals("4")) {
-						quitterProgramme();
-					} else if (!itemMenu.equals("1") && !itemMenu.equals("2") && !itemMenu.equals("3")
-							&& !itemMenu.equals("4")) {
-						choixIncorrect();
-					}
-				} while (cmd3);
+				tu.rechercherTache(tList, mList, aList);
 
 			} else if (choix.equals("4")) {
 				tu.afficherListeTache(tList);
@@ -121,7 +71,6 @@ public class TaskManager {
 		} while (cmd.toUpperCase().equals("O"));
 	}
 
-	
 	////////////////
 
 	// Methode pour quitter le programme
@@ -145,7 +94,6 @@ public class TaskManager {
 		// on retourne v
 		return v;
 	}
-
 
 	// Methode pour les choix non corrects
 	static void choixIncorrect() {
